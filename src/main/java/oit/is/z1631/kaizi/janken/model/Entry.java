dde1554a29509c42ca62d320453b3454ba24ecc4
b18021 <e1b18021@oit.ac.jp>
2020-10-20 05:45:48 +0900

    複数アカウントでログインして相互確認できた

---
+++ src/main/java/oit/is/z1631/kaizi/janken/model/Entry.java
@@ -0,0 +1,29 @@
+package oit.is.z1631.kaizi.janken.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.javassist.expr.NewArray;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Entry {
+  private List<String> users = new ArrayList<>();
+
+  public void adduser(String username) {
+    for (final String user : this.users) {
+      if (username.equals(user)) {
+        return;
+      }
+    }
+    this.users.add(username);
+  }
+
+  public List<String> getUsers() {
+    return this.users;
+  }
+
+  public void setUsers(List<String> users) {
+    this.users = users;
+  }
+}
