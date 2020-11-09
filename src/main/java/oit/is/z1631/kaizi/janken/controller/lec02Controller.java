431311628ee757cd4b01795213db2e9acef5e734
b18021 <e1b18021@oit.ac.jp>
2020-11-09 21:07:28 +0900

    CPUとの対戦をデータベースに保存できるようにした

--- src/main/java/oit/is/z1631/kaizi/janken/controller/lec02Controller.java
+++ src/main/java/oit/is/z1631/kaizi/janken/controller/lec02Controller.java
@@ -29,6 +29,9 @@ public class lec02Controller {
   private UserMapper userMapper;
   @Autowired
   private MatchMapper matchMapper;
+
+  private int userid;
+
   /*
    * @GetMapping("/lec02") public String lec03janken(Principal prin, ModelMap
    * model) { String loginUser = prin.getName(); this.entry.adduser(loginUser);
@@ -37,14 +40,14 @@ public class lec02Controller {
    */
 
   /*
-   * @GetMapping("/lec02/game") public String janken(@RequestParam Integer myhand,
+   * @GetMapping("/lec02") public String janken(@RequestParam Integer myhand,
    * ModelMap model) { int result = Janken.playjanken(myhand); String resultString
    * = ""; String myhandString = ""; switch (result) { case Janken.win:
    * resultString = "win"; break; case Janken.lose: resultString = "lose"; break;
    * case Janken.draw: resultString = "draw"; break; } switch (myhand) { case
    * Janken.GU: myhandString = "GU"; break; case Janken.PA: myhandString = "PA";
    * break; case Janken.CHOKI: myhandString = "CHOKI"; break; }
-   * 
+   *
    * model.addAttribute("result", 0 <= result); model.addAttribute("myhandString",
    * myhandString); model.addAttribute("resultString", resultString); return
    * "lec02.html"; }
@@ -75,9 +78,54 @@ public class lec02Controller {
     ArrayList<User> users = userMapper.selectAllUsers();
     String loginuser = users.get(id - 1).getName();
     String cpu = users.get(0).getName();
+    this.userid = id;
     model.addAttribute("loginuser", loginuser);
     model.addAttribute("cpu", cpu);
-
+    model.addAttribute("id", userid);
     return "match.html";
   }
+
+  @GetMapping("/game")
+  public String game(@RequestParam Integer myhand, ModelMap model, Principal prin) {
+    int result = Janken.playjanken(myhand);
+    String resultString = "";
+    String myhandString = "";
+    switch (result) {
+      case Janken.win:
+        resultString = "win";
+        break;
+      case Janken.lose:
+        resultString = "lose";
+        break;
+      case Janken.draw:
+        resultString = "draw";
+        break;
+    }
+    switch (myhand) {
+      case Janken.Gu:
+        myhandString = "GU";
+        break;
+      case Janken.Pa:
+        myhandString = "PA";
+        break;
+      case Janken.Choki:
+        myhandString = "CHOKI";
+        break;
+    }
+    model.addAttribute("result", 0 <= result);
+    model.addAttribute("myhandString", myhandString);
+    model.addAttribute("resultString", resultString);
+
+    Match match = new Match();
+    int cpu_id = 1;
+    match.setUser_1(this.userid);
+    match.setUser_2(cpu_id);
+    String userHand = myhandString;
+    String cpuHand = "Gu";
+    match.setUser_2_hand(userHand);
+    match.setUser_1_hand(cpuHand);
+    matchMapper.insertMatch(match);
+    return "match.html";
+  }
+
 }
